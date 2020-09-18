3e218044bc869f4e6a96696a23ccb411601c526c
Paul <phapp1988@gmail.com>
2020-09-18 15:31:43 -0500

    Change html title attribute

--- pages/_app.js
+++ pages/_app.js
@@ -30,7 +30,7 @@ function MyApp(props) {
   return (
     <React.Fragment>
       <Head>
-        <title>Book Trading Club</title>
+        <title>Nightlife App</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
